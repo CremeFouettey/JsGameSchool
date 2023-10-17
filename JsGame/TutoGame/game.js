71e5950b89f60b6472653ddcef3374a86854d1c7
CremeFouettey <144395762+CremeFouettey@users.noreply.github.com>
2023-10-17 14:31:02 -0400

    Update game.js

--- JsGame/TutoGame/game.js
+++ JsGame/TutoGame/game.js
@@ -11,19 +11,30 @@ class PlayScene extends Phaser.Scene
 
     preload() { //Charge les fichiers
         this.load.image("Background", "assets/ciel.png");
+        this.load.image("Stars", "assets/etoile.png");
         this.load.spritesheet("IceBlock", "assets/iceblock.png",
          {frameHeight: 48, frameWidth: 48});
+
     }
 
     create() { //Utilisée au début
         this.add.image(gameWidth/2 + 50,gameHeigt/2 - 50, "Background");
+        
         this.ground = this.physics.add.staticGroup({
             key: "IceBlock",
             frame: 0,
             repeat: 5,
             setXY: {x: 24, y: gameHeigt, stepX: 48}
-        }
-        );
+        });
+
+        const numStars = 6;
+        const step = worldWidth/(numStars+1)
+        this.stars = this. physics.add.group({
+            key: "Stars",
+            repeat: numStars-1,
+            gravityY: 300,
+            setXY: {x: step, y: -30, stepX: step}
+        });
     }
 
     update() { //Utilisée à chaque image
